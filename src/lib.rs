3131d9d5fb55b65c988c462cb0835c9e5ee14406
Kai Schmidt <kaikaliischmidt@gmail.com>
2021-05-12 15:48:27 -0700

    document everything

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,9 @@
+#![warn(missing_docs)]
+
+/*!
+`ridgeline` is a crate for simplifying frequency spectrum analysis of audio streamed from input devices.
+*/
+
 use std::{collections::VecDeque, ops::RangeBounds, sync::mpsc, usize};
 
 use crate::cpal::{
@@ -9,28 +15,37 @@ use rustfft::{num_complex::Complex, FftPlanner};
 
 pub use cpal;
 
+/// A error encountered when trying to build a [`Spectrometer`]
 #[derive(Debug, thiserror::Error)]
 pub enum BuildSpectrometerError {
+    /// An error building the audio stream
     #[error("{0}")]
     Stream(#[from] BuildStreamError),
+    /// An error starting the audio stream
     #[error("{0}")]
     Play(#[from] PlayStreamError),
+    /// An error querying stream configurations
     #[error("{0}")]
     Configs(#[from] SupportedStreamConfigsError),
+    /// A device has no available stream configurations
     #[error("No config available for device")]
     NoConfig,
+    /// No default input device is available
     #[error("No device available")]
     NoDevice,
 }
 
+/// A result type for trying to build a [`Spectrometer`]
 pub type BuildSpectrometerResult = Result<Spectrometer, BuildSpectrometerError>;
 
+/// The freqency spectrum of the input at some time
 pub struct Spectrum {
     amps: Vec<f32>,
     sample_rate: u32,
 }
 
 impl Spectrum {
+    /// Create a new silent spectrum
     pub fn silent(sample_rate: u32, max_freq: f32) -> Self {
         Spectrum {
             amps: vec![0.0; (max_freq * 2.0).round() as usize],
@@ -43,6 +58,9 @@ impl Spectrum {
     fn frequency_at(&self, i: usize) -> f32 {
         i as f32 * self.bucket_width()
     }
+    /// Get the amplitude at some frequency
+    ///
+    /// Frequencies between FFT bucket bounds are interpolated
     pub fn amplitude(&self, freq: f32) -> f32 {
         let delta = self.bucket_width();
         let ratio = freq / delta;
@@ -57,6 +75,7 @@ impl Spectrum {
         let param = ratio - floor;
         (1.0 - param) * left + param * right
     }
+    /// Get the frequency with the maximum amplitude within some range of frequencies
     pub fn max_in_range(&self, range: impl RangeBounds<f32>) -> f32 {
         let bucket = self
             .amps
@@ -70,11 +89,15 @@ impl Spectrum {
             .0;
         bucket as f32 * self.bucket_width()
     }
+    /// Get the frequency with the maximum amplitude
     pub fn max(&self) -> f32 {
         self.max_in_range(..)
     }
 }
 
+/// An iterator that produces frequency spectra from an audio input device
+///
+/// At any moment, [`Spectrometer::read`] can be called to get the [`Spectrum`] of the current input.
 pub struct Spectrometer {
     _stream: Stream,
     recv: mpsc::Receiver<f32>,
@@ -85,28 +108,43 @@ pub struct Spectrometer {
     calibration: Option<Spectrum>,
 }
 
+/**
+A builder for a [`Spectrometer`]
+
+Created with [`Spectrometer::builder`]
+
+At any moment, [`Spectrometer::read`] may be called to get the [`Spectrum`] of
+the current audio input.
+*/
 pub struct SpectrometerBuilder<'a> {
+    /// The maximum frequency can be detected
     pub max_freq: f32,
+    /// The input device to use. If not set, the default device will be used.
     pub device: Option<&'a Device>,
+    /// The stream configuration to be used. If not set, the default will be used.
     pub config: Option<SupportedStreamConfig>,
 }
 
 impl<'a> SpectrometerBuilder<'a> {
+    /// Set the maximum frequency that can be detected
     pub fn max_freq(self, max_freq: f32) -> Self {
         SpectrometerBuilder { max_freq, ..self }
     }
+    /// Set the input device
     pub fn device(self, device: &'a Device) -> Self {
         SpectrometerBuilder {
             device: Some(device),
             ..self
         }
     }
+    /// Set the stream configuration
     pub fn config(self, config: SupportedStreamConfig) -> Self {
         SpectrometerBuilder {
             config: Some(config),
             ..self
         }
     }
+    /// Build the [`Spectrometer`]
     pub fn build(self) -> BuildSpectrometerResult {
         let default_device;
         let device = if let Some(device) = self.device {
@@ -169,6 +207,7 @@ impl<'a> SpectrometerBuilder<'a> {
 }
 
 impl Spectrometer {
+    /// Create a new [`SpectrometerBuilder`]
     pub fn builder<'a>() -> SpectrometerBuilder<'a> {
         SpectrometerBuilder {
             max_freq: 4000.0,
@@ -176,15 +215,26 @@ impl Spectrometer {
             config: None,
         }
     }
+    /// Create a spectrometer using the default input device
     pub fn from_default_device(max_freq: f32) -> BuildSpectrometerResult {
         Self::builder().max_freq(max_freq).build()
     }
+    /// Get the number of samples used for frequency analysis
     pub fn sample_size(&self) -> usize {
         (self.max_freq * 2.0).round() as usize
+    }
+    /// The amount of time in seconds used to perform frequency analysis
+    ///
+    /// Note that [`Spectrometer::read`] can be called much more often that this
+    pub fn sample_period(&self) -> f32 {
+        self.sample_size() as f32 / self.sample_rate as f32
     }
     fn buffer_size(&self) -> usize {
         self.sample_size() * 2
     }
+    /// Use the next few spectra to calibrate a definition of "silence"
+    ///
+    /// All spectra created after calling this function will have the "silence" spectrum subtracted.
     pub fn calibrate(&mut self) {
         let mut new_calibration = self.raw_next();
         for _ in 0..self.sample_size() / 10 {
@@ -195,6 +245,7 @@ impl Spectrometer {
         }
         self.calibration = Some(new_calibration)
     }
+    /// Clear the calibration set by [`Spectrometer::calibrate`]
     pub fn uncalibrate(&mut self) {
         self.calibration = None;
     }
@@ -224,6 +275,16 @@ impl Spectrometer {
             sample_rate: self.sample_rate,
         }
     }
+    /// Get the [`Spectrum`] at the moment this function called
+    pub fn read(&mut self) -> Spectrum {
+        let mut spectrum = self.raw_next();
+        if let Some(min) = &self.calibration {
+            for (s, min) in spectrum.amps.iter_mut().zip(&min.amps) {
+                *s = (*s - min).max(0.0);
+            }
+        }
+        spectrum
+    }
 }
 
 impl Default for Spectrometer {
@@ -236,12 +297,6 @@ impl Default for Spectrometer {
 impl Iterator for Spectrometer {
     type Item = Spectrum;
     fn next(&mut self) -> Option<Self::Item> {
-        let mut spectrum = self.raw_next();
-        if let Some(min) = &self.calibration {
-            for (s, min) in spectrum.amps.iter_mut().zip(&min.amps) {
-                *s = (*s - min).max(0.0);
-            }
-        }
-        Some(spectrum)
+        Some(self.read())
     }
 }
